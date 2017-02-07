28b783fd7b66db66b61a28e86f2146ce98eea2f5
Joseph Toh Djojo <joseph.tohdjojo@gmail.com>
2017-02-07 16:46:27 -0500

    serving over https

--- src/services/projectsSrv.js
+++ src/services/projectsSrv.js
@@ -10,20 +10,20 @@ export default function() {
 		{
 			title: 'YENTE',
 			desc: 'This is a personal project that I built during my time at DevMountain Coding Bootcamp in Provo, UT. I was awarded "Best Overall Web App" and this project is now featured on DevMountain\'s website (www.devmounta.in/students).',
-			siteUrl: 'http://107.170.210.151/',
+			siteUrl: 'https://107.170.210.151/',
 			github: 'https://github.com/joseph-tohdjojo/Yente'
 		},
 		{
 			title: 'MY PORTFOLIO PAGE',
 			desc: 'This is my portfolio site. Check out the preview. The preview itself is a full functioning website. No <img> tags here. HIRE ME!',
-			siteUrl: 'http://joseph-tohdjojo.github.io/',
+			siteUrl: 'https://joseph-tohdjojo.github.io/',
 			github: 'https://github.com/joseph-tohdjojo/joseph-tohdjojo.github.io'
 		},
 		{
 			title: 'Random Donald Trump Quote Generator',
 			desc: 'Pretty self-explanatory.',
-			siteUrl: 'http://s.codepen.io/mrhamburger/debug/eNwxxw',
-			github: 'http://s.codepen.io/mrhamburger/debug/eNwxxw'
+			siteUrl: 'https://s.codepen.io/mrhamburger/debug/eNwxxw',
+			github: 'https://s.codepen.io/mrhamburger/debug/eNwxxw'
 		}
 	];
 
